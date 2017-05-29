c65a222e50b04905e34f96e82e459f04f8f6798c
hellcracker <gulati.rockz1@gmail.com>
2017-05-29 11:43:14 +0530

    WSGI.PY

--- commentblog/wsgi.py
+++ commentblog/wsgi.py
@@ -1,7 +1,17 @@
+"""
+WSGI config for commentblog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "commentblog.settings")
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "commentblog.settings")
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
