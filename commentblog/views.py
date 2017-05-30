d1dab162446f4e1c500cb3d929fdcbaa6fc787a4
hellcracker <gulati.rockz1@gmail.com>
2017-05-30 20:31:32 +0530

    INDEX PAGE

--- commentblog/views.py
+++ commentblog/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 
 def welcome(request):
-	return HttpResponse("<br/><h1>HELL YEAH WELCOME HERE BROTHER.</h1><br/><h1> JUST WAIT UNTILL ITS DONE... SEE YOU SOON BRUH</h1>")
+	return render(request, 'index.html')
